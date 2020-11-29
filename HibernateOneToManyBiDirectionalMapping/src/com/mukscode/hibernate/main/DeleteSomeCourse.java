3e0c636b8ee1b9986da099cd315116ca0c425ab8
Mukesh Kumar <mukku111@in.ibm.com>
2020-11-29 18:05:18 +0530

    hibernate-one-to-many-bi

---
+++ HibernateOneToManyBiDirectionalMapping/src/com/mukscode/hibernate/main/DeleteSomeCourse.java
@@ -0,0 +1,55 @@
+package com.mukscode.hibernate.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mukscode.hibernate.entity.Course;
+import com.mukscode.hibernate.entity.Instructor;
+import com.mukscode.hibernate.entity.InstructorDetail;
+
+
+public class DeleteSomeCourse {
+
+public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+		
+		//create session
+		Session session = factory.getCurrentSession();
+		
+		try 
+		{
+
+			//start transaction
+			session.beginTransaction();
+			
+			//get the course from database
+			int theId = 11;
+			Course theCourse = session.get(Course.class, theId);
+			
+			System.out.println("Course is: "+ theCourse);
+			
+			//delete the course
+			session.delete(theCourse);
+			
+			//commit the transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!!!");
+			
+		}
+		finally 
+		{
+			factory.close();
+		}
+		
+	}
+
+}
